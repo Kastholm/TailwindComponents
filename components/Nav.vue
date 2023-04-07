b552b6e4b28bfb011931bc6b296f11344dcb8ceb
Kastholm <Kastholm1995@gmail.com>
2023-04-07 15:30:02 +0200

    added styling

--- components/Nav.vue
+++ components/Nav.vue
@@ -1,8 +1,8 @@
 <template>
   <nav>
     <div
-      class="folderPath"
-      v-for="(group, groupName) in groupedRoutes"
+      :class="['folderPath', gradientClasses[index % gradientClasses.length]]"
+      v-for="(group, groupName, index) in groupedRoutes"
       :key="groupName"
     >
       <h2>{{ groupName }}</h2>
@@ -42,26 +42,47 @@ const groupedRoutes = computed(() => {
 
   return grouped;
 });
+
+const gradientClasses = [
+  "bg-gradient-to-r from-blue-500 via-teal-400 to-green-500",
+  "bg-gradient-to-r from-red-500 via-pink-400 to-purple-500",
+  "bg-gradient-to-r from-yellow-500 via-orange-400 to-red-500",
+  "bg-gradient-to-r from-green-500 via-blue-400 to-indigo-500",
+  "bg-gradient-to-r from-pink-500 via-purple-400 to-pink-500",
+  "bg-gradient-to-r from-orange-500 via-yellow-400 to-orange-500",
+  // Add more gradient classes here if needed
+];
 </script>
 
 <style scoped>
+nav {
+  @apply bg-gradient-to-r from-gray-800 via-gray-900 to-black py-12;
+}
 .folderPath {
-  background: rgba(214, 190, 9, 0.703);
-  display: flex;
-  flex-direction: column;
+  display: grid;
+  border-radius: 30px;
+  width: 70vw;
+  max-width: 1200px;
+  place-content: center;
   align-items: flex-start;
+  text-align: center;
+  margin: 0 auto;
   margin-bottom: 2rem;
   padding: 2em;
 }
 button {
   margin: 0.5em;
+  @apply text-xl;
 }
 h2 {
-  @apply text-2xl;
+  @apply text-4xl font-bold;
+
   margin-bottom: 1em;
 }
 ul {
   display: grid;
-  grid-template-columns: 1fr 1fr 1fr 1fr;
+  place-content: center;
+  margin: 0 auto;
+  grid-template-columns: auto auto auto auto;
 }
 </style>
